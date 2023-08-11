06035f6c9c1a6119b69984ebe6c9ff2c32f41f02
workandplayhard <ethunlurk@gmail.com>
2023-08-12 03:27:14 +0800

    fixed: data sql schema

--- db/data.sql
+++ db/data.sql
@@ -92,7 +92,7 @@ CREATE TABLE `Product` (
 
 LOCK TABLES `Product` WRITE;
 /*!40000 ALTER TABLE `Product` DISABLE KEYS */;
-INSERT INTO `Product` VALUES (1,'Hat','t','Active'),(2,'Shoes','t','Active'),(3,'Pants','t','Active'),(4,'Shirt','t','InActive'),(5,'Coat','t','InActive');
+INSERT INTO `Product` VALUES (1,'Hat','https://loremflickr.com/320/240','Active'),(2,'Shoes','https://loremflickr.com/320/240','Active'),(3,'Pants','https://loremflickr.com/320/240','Active'),(4,'Shirt','https://loremflickr.com/320/240','InActive'),(5,'Coat','https://loremflickr.com/320/240','InActive');
 /*!40000 ALTER TABLE `Product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
